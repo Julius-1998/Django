111e94be939b701c4671200ee34357afa8568d1c
Julius-1998 <89859253+Julius-1998@users.noreply.github.com>
2022-01-13 16:34:29 -0500

    Homepage, Login, register modules. Safety check added.

--- homepage/views.py
+++ homepage/views.py
@@ -4,10 +4,12 @@ from django.contrib.auth.models import User
 
 
 def homepage(request):
+    # homepage view
     if request.user.is_authenticated:
+        # if the user logged in, show the firstname of user
+        # otherwise, show the register and login button
         is_logged_in = True
         first_name = request.user.username
-        print(request.user.username)
         return render(request, 'homepage/homepage.html', {'is_logged_in': is_logged_in, 'first_name': first_name})
     else:
         return render(request, 'homepage/homepage.html')
--- login/views.py
+++ login/views.py
@@ -7,11 +7,14 @@ from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from django.http import Http404
+from django.contrib import messages
 
 import homepage.views
 
 
 def login_view(request):
+    # if method is get, return static login webpage
+    # if method is post, login the user, raise exception if the password is incorrect or user does not exist
     if request.method == 'GET':
         return render(request, 'login/login.html')
     if request.method == 'POST':
@@ -32,18 +35,25 @@ def logout_view(request):
 
 
 def register_view(request):
+    # if the method is get, return static register webpage
+    # if the method is post, the user info is posted in the form, register it
     if request.method == 'GET':
         return render(request, 'login/register.html')
     if request.method == 'POST':
-        # TODO: check if the user already exists
         username = request.POST.get("username")
         password = request.POST.get("password")
         first_name = request.POST.get("firstName")
         last_name = request.POST.get("lastName")
         email = request.POST.get("email")
+        # Check if the user already exists
+        if User.objects.filter(username=username).exists():
+            messages.warning(request, 'Username already in use')
+            return render(request, 'login/register.html')
         user = User.objects.create_user(username, email, password)
         user.first_name = first_name
         user.last_name = last_name
         user.save()
+        # login the user after register
+        login(request, user)
         return redirect(homepage.views.homepage)
 
